c2de0845fcc74a9c4cf8ef192f25c378e5e81e67
root <rparthiban1612@gmail.com>
2024-04-27 19:08:35 +0530

    reset added

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -1,11 +1,23 @@
 import { Container } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from "@mui/material";
+import { RiFileList3Line } from 'react-icons/ri';
+
 
 export default function Settings() {
   return (
     <Container sx={{ padding: 1 }}>
       <h3>Settings</h3>
+      <Button
+        className="w-50"
+        variant="contained"
+        startIcon={<RiFileList3Line />}
+        onClick={() => localStorage?.clear()}
+      >
+        Reset list
+      </Button>
+      <p></p>
       <Link to={"/about-us"}>About us</Link>
     </Container>
   )
