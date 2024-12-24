fd02f385215fbd9adf9e83782430c33f213815be
Parthiban1612 <parthi.webdev@gmail.com>
2024-12-24 11:19:11 +0530

    added new onesssssssssssssss

--- src/lib/convertRuppee.js
+++ src/lib/convertRuppee.js
@@ -1,4 +1,4 @@
 export function formatRupees(amount) {
-  const validAmount = Number(amount) || 0; // Convert to number or default to 0 if conversion fails
+  const validAmount = Number(amount) || 0;
   return validAmount.toLocaleString('en-IN', { minimumFractionDigits: 0, maximumFractionDigits: 2 });
 }
